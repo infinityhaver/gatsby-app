19e7cb2480c7b64adae40d6aba1c32eb57df9a38
Nathanael Gray <ngray@forefrontweb.com>
2022-02-03 14:55:58 -0500

    trying to trigger deployment to gatsby cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `http://gatsby.local/graphql`,
+          `https://source.lonelycoconut.com/graphql`,
       },
     },
 
